827ea80b56f112d1d80a39084b793a883a42f8a7
Brian W Bush <brian.bush@nrel.gov>
2019-05-11 11:49:30 -0600

    ran nesta cases with PowerModels.jl

---
+++ simulations/nesta-static.jl
@@ -0,0 +1,63 @@
+# Set working directory.
+
+if isdefined(Main, :SIIP_TDAPS_DIR)
+    cd(SIIP_TDAPS_DIR)
+end
+
+
+# Set-up packages and paths.
+
+THE_ENV = "powermodels.env"
+include("setup-powermodels.jl")
+
+
+# Create and solve the model using PowerSimulations.
+
+nesta_summary = DataFrame(
+    File=String[],
+    Model=String[],
+    Buses=Int64[],
+    Branches=Int64[],
+    Algorithm=String[],
+    Solver=String[],
+    Time=Float64[],
+    Status=Symbol[],
+)
+
+for case_data in NESTA_MODELS
+
+    @info string("Solving ", case_data, " . . .")
+
+    case_soln = run_dc_opf(case_data, IpoptSolver())
+    @info string(" . . . DC ", case_soln["status"])
+    push!(nesta_summary, (
+        case_data,
+        case_soln["data"]["name"],
+        case_soln["data"]["bus_count"],
+        case_soln["data"]["branch_count"],
+        "DCPF",
+        case_soln["solver"],
+        case_soln["solve_time"],
+        case_soln["status"],
+    ))
+
+    case_soln = run_dc_opf(case_data, IpoptSolver())
+    @info string(" . . . DC ", case_soln["status"])
+    push!(nesta_summary, (
+        case_data,
+        case_soln["data"]["name"],
+        case_soln["data"]["bus_count"],
+        case_soln["data"]["branch_count"],
+        "ACPF",
+        case_soln["solver"],
+        case_soln["solve_time"],
+        case_soln["status"],
+    ))
+
+end
+
+# Record the results.
+
+print(nesta_summary)
+
+CSV.write("nesta-static.tsv", nesta_summary, delim="\t")
