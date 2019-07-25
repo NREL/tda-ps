fc96dcc1864cf04ad339d2f55921a3472e5b9afb
Brian Bush <bbush@el1.ib0.cm.hpc.nrel.gov>
2019-07-25 07:46:49 -0600

    scripts for downloading results from aws

---
+++ contingency-datasets/fetch-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+wget -O study-01/nesta_case118_ieee/results.zip  https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-01/nesta_case118_ieee/results.zip
+wget -O study-01/nesta_case30_ieee/results.zip   https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-01/nesta_case30_ieee/results.zip
+wget -O study-02/nesta_case118_ieee/results.zip  https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-02/nesta_case118_ieee/results.zip
+wget -O study-02/nesta_case118_ieee/unique.zip   https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-02/nesta_case118_ieee/unique.zip
+wget -O study-02/nesta_case30_ieee/results.zip   https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-02/nesta_case30_ieee/results.zip
+wget -O study-02/nesta_case30_ieee/unique.zip    https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-02/nesta_case30_ieee/unique.zip
+wget -O study-02/nesta_case3120sp_mp/results.zip https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-02/nesta_case3120sp_mp/results.zip
+wget -O study-03/nesta_case118_ieee/results.zip  https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-03/nesta_case118_ieee/results.zip
+wget -O study-03/nesta_case30_ieee/results.zip   https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-03/nesta_case30_ieee/results.zip
+wget -O study-03/nesta_case30_ieee/unique.zip    https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-03/nesta_case30_ieee/unique.zip
+wget -O study-03/nesta_case3120sp_mp/results.zip https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-03/nesta_case3120sp_mp/results.zip
+wget -O study-05/nesta_case118_ieee/results.zip  https://nrel-demos.s3-us-west-2.amazonaws.com/tda-ps/study-05/nesta_case118_ieee/results.zip
