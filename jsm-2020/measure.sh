4368c859e702cbbf39daab7a1dd3d0c3ddba4b51
Brian W Bush <brian.bush@nrel.gov>
2020-05-13 15:32:30 -0600

    generalized measurement script

--- jsm-2020/measure.sh
+++ jsm-2020/measure.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-. /opt/miniconda3a/etc/profile.d/conda.sh
+. $HOME/local/miniconda3/etc/profile.d/conda.sh
 conda activate tda
 
-for d in 31 32 33 34 35 36 37 38
+date
+
+for d in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 do
     python measure.py $d &
 done
