1eb99654b01c1be023ac0f422e02de9c581d9f5e
Brian W Bush <brian.bush@nrel.gov>
2020-07-19 09:55:06 -0600

    further revised sampling

--- jsm-2020/script.sh
+++ jsm-2020/script.sh
@@ -5,25 +5,25 @@ do
     $HOME/local/julia-1.3.1/bin/julia -e 'FIRST_CASE = 1'$d'100000;'                \
                                       -e 'OUTPUT_DIR = "'$d'";'                     \
                                       -e 'DEVICE_TYPES = [:Line];'                  \
-                                      -e 'RADII = sample(0:26, 25, replace=false);' \
+                                      -e 'RADII = vcat([i:5:25 for i in 0:4]...);'  \
                                       -e 'FRACTIONS = [1, 2, 5, 10, 20, 50, 100];'  \
-                                      -e 'SAMPLE_SIZE = 1250;'                      \
+                                      -e 'SAMPLE_SIZE = 625;'                       \
                                       -L script.jl |& tee $d/Line.log               &
     sleep 150s
     $HOME/local/julia-1.3.1/bin/julia -e 'FIRST_CASE = 1'$d'200000;'                \
                                       -e 'OUTPUT_DIR = "'$d'";'                     \
                                       -e 'DEVICE_TYPES = [:Transformer2W];'         \
-                                      -e 'RADII = sample(0:26, 25, replace=false);' \
+                                      -e 'RADII = vcat([i:5:25 for i in 0:4]...);'  \
                                       -e 'FRACTIONS = [1, 2, 5, 10, 20, 50, 100];'  \
-                                      -e 'SAMPLE_SIZE = 1250;'                      \
+                                      -e 'SAMPLE_SIZE = 625;'                       \
                                       -L script.jl |& tee $d/Transformer2W.log      &
     sleep 150s
     $HOME/local/julia-1.3.1/bin/julia -e 'FIRST_CASE = 1'$d'300000;'                \
                                       -e 'OUTPUT_DIR = "'$d'";'                     \
                                       -e 'DEVICE_TYPES = [:Line, :Transformer2W];'  \
-                                      -e 'RADII = sample(0:26, 25, replace=false);' \
+                                      -e 'RADII = vcat([i:5:25 for i in 0:4]...);'  \
                                       -e 'FRACTIONS = [1, 2, 5, 10, 20, 50, 100];'  \
-                                      -e 'SAMPLE_SIZE = 1250;'                      \
+                                      -e 'SAMPLE_SIZE = 625;'                       \
                                       -L script.jl |& tee $d/Line,Transformer2W.log &
     sleep 150s
 done
