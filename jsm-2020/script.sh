28426cdeb45523cd27903026e57be6fb73f3de46
Brian W Bush <brian.bush@nrel.gov>
2020-07-19 09:08:45 -0600

    revised sampling strategy

--- jsm-2020/script.sh
+++ jsm-2020/script.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
-for d in 00 01 02 03 04 05 06 07 08
+for d in 00 01 02 03 04 05 06 07
 do
     $HOME/local/julia-1.3.1/bin/julia -e 'FIRST_CASE = 1'$d'100000;'                \
                                       -e 'OUTPUT_DIR = "'$d'";'                     \
                                       -e 'DEVICE_TYPES = [:Line];'                  \
                                       -e 'RADIUS = 25;'                             \
-                                      -e 'SAMPLE_SIZE = 625;'                       \
+                                      -e 'FRACTIONS = [1, 2, 5, 10, 20, 50, 100];'  \
+                                      -e 'SAMPLE_SIZE = 1250;'                      \
                                       -L script.jl |& tee $d/Line.log               &
     sleep 150s
     $HOME/local/julia-1.3.1/bin/julia -e 'FIRST_CASE = 1'$d'200000;'                \
                                       -e 'OUTPUT_DIR = "'$d'";'                     \
                                       -e 'DEVICE_TYPES = [:Transformer2W];'         \
                                       -e 'RADIUS = 25;'                             \
-                                      -e 'SAMPLE_SIZE = 625;'                       \
+                                      -e 'FRACTIONS = [1, 2, 5, 10, 20, 50, 100];'  \
+                                      -e 'SAMPLE_SIZE = 1250;'                      \
                                       -L script.jl |& tee $d/Transformer2W.log      &
     sleep 150s
     $HOME/local/julia-1.3.1/bin/julia -e 'FIRST_CASE = 1'$d'300000;'                \
                                       -e 'OUTPUT_DIR = "'$d'";'                     \
                                       -e 'DEVICE_TYPES = [:Line, :Transformer2W];'  \
                                       -e 'RADIUS = 25;'                             \
-                                      -e 'SAMPLE_SIZE = 625;'                       \
+                                      -e 'FRACTIONS = [1, 2, 5, 10, 20, 50, 100];'  \
+                                      -e 'SAMPLE_SIZE = 1250;'                      \
                                       -L script.jl |& tee $d/Line,Transformer2W.log &
     sleep 150s
 done
